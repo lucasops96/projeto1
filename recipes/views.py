d993061110b67b3e3a5172104e2b2f27a90da85d
João Lucas de Sousa Martins <lucasbanana96@gmail.com>
2023-08-14 16:12:22 -0300

    header created

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def home(request):
-    return render(request,'recipes/home.html', context={
+    return render(request,'recipes/pages/home.html', context={
         'name':'Miguel Santos'
     })
 
-def contato(request):
-    return render(request,'recipes/contato.html')
-
-def sobre(request):
-    return HttpResponse('Contato do User')
