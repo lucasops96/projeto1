c21790b3a15ee1ab48543ab70703ba821e0611c6
João Lucas de Sousa Martins <lucasbanana96@gmail.com>
2023-08-17 10:52:50 -0300

    Injetando dados view Home

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from utils.recipes.factory import make_recipe
+
 
 def home(request):
     return render(request,'recipes/pages/home.html', context={
-        'name':'Miguel Santos'
+        'recipes':[make_recipe() for _ in range(10)],
     })
 
 def recipe(request, id):
     return render(request,'recipes/pages/recipe-view.html', context={
-        'name':'Miguel Santos'
+        'recipe':make_recipe(),
     })
