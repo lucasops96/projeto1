bbd91cbfa0cfe94f045869b769f21e711cc64271
João Lucas de Sousa Martins <lucasbanana96@gmail.com>
2023-08-18 15:32:04 -0300

    carregando dados nas views

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 
+from recipes.models import Recipe
 from utils.recipes.factory import make_recipe
 
 
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request,'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
     })
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id)
+    return render(request,'recipes/pages/home.html', context={
+        'recipes': recipes,
+    })
+
 
 def recipe(request, id):
     return render(request,'recipes/pages/recipe-view.html', context={
