f480007a0a2ca16a4a04f209b9a8e3c160dd321a
João Lucas de Sousa Martins <lucasbanana96@gmail.com>
2023-08-19 16:27:08 -0300

    page category

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 
 from recipes.models import Recipe
@@ -5,15 +6,20 @@ from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request,'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id)
-    return render(request,'recipes/pages/home.html', context={
+    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+
+    if not recipes:
+        raise Http404('Not found 😢')
+
+    return render(request,'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category |',
     })
 
 
