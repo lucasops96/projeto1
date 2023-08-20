cdae6071136f87ceef494444f3946b94ec5317fa
João Lucas de Sousa Martins <lucasbanana96@gmail.com>
2023-08-20 14:28:54 -0300

    rename projeto

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import get_list_or_404, render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
 from utils.recipes.factory import make_recipe
@@ -26,7 +26,9 @@ def category(request, category_id):
 
 
 def recipe(request, id):
+    recipe = get_object_or_404(Recipe,pk=id, is_published=True) 
+
     return render(request,'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
