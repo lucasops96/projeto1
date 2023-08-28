6f78fd9df12613ee9dfb928e2272fb717b77c401
João Lucas de Sousa Martins <lucasbanana96@gmail.com>
2023-08-27 23:26:49 -0300

    paginator in template

--- recipes/views.py
+++ recipes/views.py
@@ -4,18 +4,29 @@ from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
-from utils.recipes.factory import make_recipe
+from utils.pagination import make_pagination_range
 
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
 
-    current_page = request.GET.get('page',1)
+    try:
+        current_page = int(request.GET.get('page',1))
+    except:
+        current_page = 1
+
     paginator = Paginator(recipes, 9)
     page_obj = paginator.get_page(current_page)
 
+    pagination_range = make_pagination_range(
+        paginator.page_range,
+        4,
+        current_page,
+    )
+
     return render(request,'recipes/pages/home.html', context={
         'recipes': page_obj,
+        'pagination_range': pagination_range,
     })
 
 def category(request, category_id):
