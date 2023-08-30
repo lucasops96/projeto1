f0be1206a118f9a3b290fb6586af0e5c0b6e12f5
lucasops96 <lucasbanana96@gmail.com>
2023-08-30 11:58:59 -0300

    update register form  authors

--- authors/forms.py
+++ authors/forms.py
@@ -29,31 +29,43 @@ class RegisterForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         add_placeholder(self.fields['username'], 'Your username')
         add_placeholder(self.fields['email'], 'Your e-mail')
-        add_placeholder(self.fields['first_name'], 'Ex.: Jonh')
+        add_placeholder(self.fields['first_name'], 'Ex.: John')
         add_placeholder(self.fields['last_name'], 'Ex.: Doe')
-        add_attr(self.fields['username'],'css','a-css-class')
+        add_placeholder(self.fields['password'], 'Type your password')
+        add_placeholder(self.fields['password2'], 'Repeat your password')
+    
+    first_name = forms.CharField(
+        error_messages={'required': 'Write your first name'},
+        label='First name'
+    )
+    last_name = forms.CharField(
+        error_messages={'required': 'Write your last name'},
+        label='Last name'
+    )
+    email = forms.EmailField(
+        error_messages={'required': 'E-mail is required'},
+        label='E-mail',
+        help_text='The e-mail must be valid.',
+    )
 
     password = forms.CharField(
-        required=True,
-        widget= forms.PasswordInput(attrs={
-            'placeholder':'Your password.',
-        }),
+        widget= forms.PasswordInput(),
         error_messages={'required':'Password must not  be empty'},
         help_text=(
             'Password must have at least one uppercase letter, '
             'one lowercase letter and one number. The length should be '
             'at least 8 characters.'
         ),
-        validators=[strong_password]
+        validators=[strong_password],
+        label='Password',
     )
 
     password2 =  forms.CharField(
-        required=True,
-        widget= forms.PasswordInput(attrs={
-            'placeholder':'Repeat your password.',
-            
-        }),
-        error_messages={'required':'Password must not  be empty'},
+        widget= forms.PasswordInput(),
+        label='Password2',
+        error_messages={
+            'required': 'Please, repeat your password'
+        },
     )
 
     class Meta:
@@ -69,47 +81,36 @@ class RegisterForm(forms.ModelForm):
 
         labels = {
             'username':'Username',
-            'first_name':'First name',
-            'last_name':'Last name',
-            'email': 'E-mail',
-            'password': 'Password',
-        }
-
-        help_texts = {
-            'email':'The e-mail must be  valid.',
         }
 
         error_messages = {
             'username':{
                 'required':'This field must not  be empty',
             },
-            'password':{
-                'required':'Password field must not  be empty',
-            },
             
         }
 
-        widgets = {
-            'first_name': forms.TextInput(attrs={
-                'placeholder':'Type your first name here',
-                'class':'input text-input',
-            }),
-            'password': forms.PasswordInput(attrs={
-                'placeholder':'Type your password here',
-            })
-        }
+        # widgets = {
+        #     'first_name': forms.TextInput(attrs={
+        #         'placeholder':'Type your first name here',
+        #         'class':'input text-input',
+        #     }),
+        #     'password': forms.PasswordInput(attrs={
+        #         'placeholder':'Type your password here',
+        #     })
+        # }
     
-    def clean_password(self):
-        data = self.cleaned_data.get('password')
+    # def clean_password(self):
+    #     data = self.cleaned_data.get('password')
 
-        if 'oi' in data:
-            raise ValidationError(
-                'Não digite %(value)s no campo password',
-                code='invalid',
-                params={'value':'"oi"'}
-            )
+    #     if 'oi' in data:
+    #         raise ValidationError(
+    #             'Não digite %(value)s no campo password',
+    #             code='invalid',
+    #             params={'value':'"oi"'}
+    #         )
 
-        return data
+    #     return data
     
     def clean(self):
         cleaned_data =  super().clean()
