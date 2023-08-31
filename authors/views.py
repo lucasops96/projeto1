944f5a9c7bc2ebad2831e032de20ca2034aacf55
João Lucas de Sousa Martins <lucasbanana96@gmail.com>
2023-08-31 15:55:08 -0300

    create LoginForm

--- authors/views.py
+++ authors/views.py
@@ -3,7 +3,7 @@ from django.http import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from .forms import RegisterForm
+from .forms import LoginForm, RegisterForm
 
 
 def register_view(request):
@@ -12,7 +12,7 @@ def register_view(request):
 
     return render(request,'authors/pages/register_view.html',{
         'form':form,
-        'form_action':reverse('authors:create'),
+        'form_action':reverse('authors:register_create'),
     })
 
 
@@ -33,3 +33,14 @@ def register_create(request):
         del(request.session['register_form_data'])
 
     return redirect('authors:register')
+
+
+def login_view(request):
+    form = LoginForm()
+    return render(request,'authors/pages/login.html',{
+        'form': form,
+        'form_action':reverse('authors:login_create'),
+    })
+
+def login_create(request):
+    return render(request,'authors/pages/login.html')
