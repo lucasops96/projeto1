e5fb414bdddcca7755ad54e679ccca68648b63e1
João Lucas de Sousa Martins <lucasbanana96@gmail.com>
2023-10-01 20:58:19 -0300

    criado class view RecipeListViewBase

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,47 @@
 import os
+from typing import Any
 
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.views.generic import ListView
 
 from recipes.models import Recipe
 from utils.pagination import make_pagination
 
 PER_PAGE = int(os.environ.get('PER_PAGE',6))
+
+class RecipeListViewBase(ListView):
+    template_name = 'recipes/pages/home.html'
+    model = Recipe
+    context_object_name = 'recipes'
+    paginate_by = None
+    ordering = ['-id']
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset()
+        qs = qs.filter(
+            is_published = True,
+        )
+        return qs
+    
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        page_obj, pagination_range = make_pagination(
+            self.request,
+            ctx.get('recipes'),
+            PER_PAGE
+        )
+
+        ctx.update({
+            'recipes':page_obj,
+            'pagination_range':pagination_range
+        })
+
+        return ctx
+
+    
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
